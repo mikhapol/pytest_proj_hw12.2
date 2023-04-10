1f2814c30ed0300bdeddb20c866073f334626601
Mikhail Polyakov PyCharm <mikhapol@bk.ru>
2023-04-10 15:37:46 +0300

    Создал файл test_dicts

---
+++ tests/test_dicts.py
@@ -0,0 +1,8 @@
+from utils.dicts import get_val
+
+
+def test_get_val():
+    assert get_val({"vcs": "mercurial"}, "vcs") == ["mercurial"]
+    assert get_val({"vcs": "mercurial"}, "vcs", "git") == ["mercurial"]
+    assert get_val({}, "vcs", "git") == ["git"]
+    assert get_val({}, "vcs", "bazaar") == ["bazaar"]
