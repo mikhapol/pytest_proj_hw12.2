d7aba448d364ed5ddda2341d77787e245c20da25
Mikhail Polyakov PyCharm <mikhapol@bk.ru>
2023-04-11 14:04:29 +0300

    Добавил второй вариант функции

--- utils/dicts.py
+++ utils/dicts.py
@@ -6,3 +6,10 @@ def get_val(collection, key, default='git'):
         return collection["vcs"]
     else:
         return default
+
+
+def get_val_v2(collection: dict, key, default='git'):
+    """Функция возвращает значение из словаря по переданному ключу, если ключ существует. В ином случае возвращается
+    значение default."""
+
+    return collection.get(key, default)
