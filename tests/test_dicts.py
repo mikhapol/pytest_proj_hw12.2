0488b91833b97746a4ef465a8a392772e64a9b99
Mikhail Polyakov PyCharm <mikhapol@bk.ru>
2023-04-11 14:05:14 +0300

    Добавил тест второй функции и информацию об отчётах.

--- tests/test_dicts.py
+++ tests/test_dicts.py
@@ -1,4 +1,4 @@
-from utils.dicts import get_val
+from utils.dicts import get_val, get_val_v2
 
 
 def test_get_val():
@@ -6,3 +6,21 @@ def test_get_val():
     assert get_val({"vcs": "mercurial"}, "vcs", "git") == "mercurial"
     assert get_val({}, "vcs", "git") == "git"
     assert get_val({}, "vcs", "bazaar") == "bazaar"
+
+
+def test_get_val_v2():
+    assert get_val_v2({"vcs": "mercurial"}, "vcs") == "mercurial"
+    assert get_val_v2({"vcs": "mercurial"}, "vcs", "git") == "mercurial"
+    assert get_val_v2({}, "vcs", "git") == "git"
+    assert get_val_v2({}, "vcs", "bazaar") == "bazaar"
+
+
+# Можно дополнительно формировать отчет в формате html:
+# https://pytest-cov.readthedocs.io/en/latest/reporting.html
+
+# pytest
+#         --cov-report html
+#         --cov-report xml
+#         --cov-report lcov
+#         --cov-report annotate
+#         --cov=myproj tests/
